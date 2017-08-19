ed59fcce3115e9cde49144099b7a53e4989739cb
uthrakumarvs <uthrakumarvs@gmail.com>
2017-08-20 02:08:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var ele=document.getElementById('title');
 ele.innerHTML='yov yo';
+var img=document.getElementById('pic');
+img.onclick=function(){
+    img.style.marginleft='100px';
+};
