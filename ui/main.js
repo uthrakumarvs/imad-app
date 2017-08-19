23345da3888d8b646bdc71d827b427f2ced110bc
uthrakumarvs <uthrakumarvs@gmail.com>
2017-08-20 02:17:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,10 @@ console.log('Loaded!');
 var ele=document.getElementById('title');
 ele.innerHTML='yov yo';
 var img=document.getElementById('pic');
-var moveright = function(){
+function moveright(){
   moveLeft=moveLeft+10;
   img.style.marginLeft= marginLeft+ px;
-};
+}
 img.onclick = function() {
     var interval = setInterval(moveright,100);
 };
