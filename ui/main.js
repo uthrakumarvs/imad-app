9a577880a889348b4615d2db72407e55b8b5ef1f
uthrakumarvs <uthrakumarvs@gmail.com>
2017-08-20 02:20:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,13 +6,10 @@ ele.innerHTML='yov yo';
 var img=document.getElementById('pic');
 
 var marginLeft=0;
-
 function moveright(){
-  marginLeft=marginLeft+10;
+  marginLeft=marginLeft+1;
   img.style.marginLeft= marginLeft+ 'px';
 }
-
-
 img.onclick = function() {
-    var interval = setInterval(moveright,100);
+    var interval = setInterval(moveright,50);
 };
