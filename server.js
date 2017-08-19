d6e04f38164a95127b3efed129303aab39058ee8
uthrakumarvs <uthrakumarvs@gmail.com>
2017-08-19 19:35:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/article1', function(req,res)
 {
-   res.send('article1 handled here'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article1.html')); 
 });
 
 app.get('/article2', function(req,res)
